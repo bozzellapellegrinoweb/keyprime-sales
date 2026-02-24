e5695166c4980a6e7d669aa8175fcf6e1ec7ffc0
bozzellapellegrinoweb <bozzellapellegrino@gmail.com>
2026-02-24 12:20:04 +0400

    Update send-push.js

--- api/send-push.js
+++ api/send-push.js
@@ -7,14 +7,15 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
   const { title, message, url } = req.body;
   try {
-    const response = await fetch('https://onesignal.com/api/v1/notifications', {
+    const response = await fetch('https://api.onesignal.com/notifications', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Basic os_v2_app_a4onfq2khvgs7dpl7jlvv3cxrxrqsndhpffui24npxugdjqnltcduawer4odsts27y7lwhao6uzdyqamepmoeattxq7hdtvc3k6khvq'
+        'Authorization': 'Key os_v2_app_a4onfq2khvgs7dpl7jlvv3cxrxrqsndhpffui24npxugdjqnltcduawer4odsts27y7lwhao6uzdyqamepmoeattxq7hdtvc3k6khvq'
       },
       body: JSON.stringify({
         app_id: '071cd2c3-4a3d-4d2f-8deb-fa575aec578d',
+        target_channel: 'push',
         contents: { en: message },
         headings: { en: title },
         included_segments: ['All'],
